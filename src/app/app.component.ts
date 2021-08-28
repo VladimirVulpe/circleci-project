04626759e7f1e01c9b397e7e80eed3d33b9d0105
Vladimir Vulpe <vladimir.vulpe@outlook.com>
2021-08-28 18:21:15 +0300

    second flush

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  
 })
 export class AppComponent {
   title = 'circleci-project';
+  btnVal = 'hello world'
+
+  public setButtonText(newTitle: string) {
+    this.btnVal = newTitle;
+  }
 }
